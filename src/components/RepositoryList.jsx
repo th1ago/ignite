b786a6710c3499c64b7119af1c67f67aaa8e068f
Thiago <thiago_murakami@hotmail.com>
2021-11-16 16:56:22 -0300

    propriedade no react

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,20 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Form for reaction',
+    link: 'https://github.com/th1ago'
+}
+
 export function RepositoryList() {
     return (
         <section className="repository-list">
             <h1>Lista de repositorio</h1>
 
             <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositorio
-                    </a>
-                </li>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
 
         </section>
